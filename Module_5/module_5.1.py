a05d230379654faa8cdcea4ebcee65766d48ad43
Aleksei <krasnobaev07@inbox.ru>
2024-09-26 20:32:54 +0500

    Second Commit. Use ctrl + alt + I

--- Module_5/module_5.1.py
+++ Module_5/module_5.1.py
@@ -3,11 +3,11 @@ class House:
         self.name = name
         self.number_of_floor = number_of_floor
     def go_to(self, new_floor):
-            if 1 <= new_floor <= self.number_of_floor:
-                for i in range(new_floor):
-                    print(i+1)
-            else:
-                print("Такого этажа не существует в этом доме")
+        if 1 <= new_floor <= self.number_of_floor:
+            for i in range(new_floor):
+                print(i+1)
+        else:
+            print("Такого этажа не существует в этом доме")
 
 # if __name__ == "__main":
 h1 = House('ЖК Горский', 18)
